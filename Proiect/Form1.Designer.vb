22f37ee4f34461184be95aa588d500ce4e8306bf
BujdeiMihai <mibujdei@stud.usv.ro>
2016-06-13 16:55:09 +0300

    Pag

---
+++ Proiect/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.JocNouToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ManualToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReguliToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BancnoteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BaniToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeiToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeiToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeiToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeiToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SetareValoareToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InformatiiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.IesireToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.JocNouToolStripMenuItem, Me.ManualToolStripMenuItem, Me.InformatiiToolStripMenuItem, Me.IesireToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(548, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'JocNouToolStripMenuItem
+        '
+        Me.JocNouToolStripMenuItem.Name = "JocNouToolStripMenuItem"
+        Me.JocNouToolStripMenuItem.Size = New System.Drawing.Size(62, 20)
+        Me.JocNouToolStripMenuItem.Text = "Joc Nou"
+        '
+        'ManualToolStripMenuItem
+        '
+        Me.ManualToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ReguliToolStripMenuItem, Me.BancnoteToolStripMenuItem, Me.SetareValoareToolStripMenuItem1})
+        Me.ManualToolStripMenuItem.Name = "ManualToolStripMenuItem"
+        Me.ManualToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.ManualToolStripMenuItem.Text = "Manual"
+        '
+        'ReguliToolStripMenuItem
+        '
+        Me.ReguliToolStripMenuItem.Name = "ReguliToolStripMenuItem"
+        Me.ReguliToolStripMenuItem.Size = New System.Drawing.Size(147, 22)
+        Me.ReguliToolStripMenuItem.Text = "Reguli"
+        '
+        'BancnoteToolStripMenuItem
+        '
+        Me.BancnoteToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BaniToolStripMenuItem, Me.LeuToolStripMenuItem, Me.LeiToolStripMenuItem, Me.LeiToolStripMenuItem1, Me.LeiToolStripMenuItem2, Me.LeiToolStripMenuItem3, Me.LeiToolStripMenuItem4})
+        Me.BancnoteToolStripMenuItem.Name = "BancnoteToolStripMenuItem"
+        Me.BancnoteToolStripMenuItem.Size = New System.Drawing.Size(147, 22)
+        Me.BancnoteToolStripMenuItem.Text = "Bancnote"
+        '
+        'BaniToolStripMenuItem
+        '
+        Me.BaniToolStripMenuItem.Name = "BaniToolStripMenuItem"
+        Me.BaniToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.BaniToolStripMenuItem.Text = "50 bani"
+        '
+        'LeuToolStripMenuItem
+        '
+        Me.LeuToolStripMenuItem.Name = "LeuToolStripMenuItem"
+        Me.LeuToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.LeuToolStripMenuItem.Text = "1 leu"
+        '
+        'LeiToolStripMenuItem
+        '
+        Me.LeiToolStripMenuItem.Name = "LeiToolStripMenuItem"
+        Me.LeiToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.LeiToolStripMenuItem.Text = "5 lei"
+        '
+        'LeiToolStripMenuItem1
+        '
+        Me.LeiToolStripMenuItem1.Name = "LeiToolStripMenuItem1"
+        Me.LeiToolStripMenuItem1.Size = New System.Drawing.Size(112, 22)
+        Me.LeiToolStripMenuItem1.Text = "50 lei "
+        '
+        'LeiToolStripMenuItem2
+        '
+        Me.LeiToolStripMenuItem2.Name = "LeiToolStripMenuItem2"
+        Me.LeiToolStripMenuItem2.Size = New System.Drawing.Size(112, 22)
+        Me.LeiToolStripMenuItem2.Text = "100 lei"
+        '
+        'LeiToolStripMenuItem3
+        '
+        Me.LeiToolStripMenuItem3.Name = "LeiToolStripMenuItem3"
+        Me.LeiToolStripMenuItem3.Size = New System.Drawing.Size(112, 22)
+        Me.LeiToolStripMenuItem3.Text = "200 lei "
+        '
+        'LeiToolStripMenuItem4
+        '
+        Me.LeiToolStripMenuItem4.Name = "LeiToolStripMenuItem4"
+        Me.LeiToolStripMenuItem4.Size = New System.Drawing.Size(112, 22)
+        Me.LeiToolStripMenuItem4.Text = "500 lei "
+        '
+        'SetareValoareToolStripMenuItem1
+        '
+        Me.SetareValoareToolStripMenuItem1.Name = "SetareValoareToolStripMenuItem1"
+        Me.SetareValoareToolStripMenuItem1.Size = New System.Drawing.Size(147, 22)
+        Me.SetareValoareToolStripMenuItem1.Text = "Setare valoare"
+        '
+        'InformatiiToolStripMenuItem
+        '
+        Me.InformatiiToolStripMenuItem.Name = "InformatiiToolStripMenuItem"
+        Me.InformatiiToolStripMenuItem.Size = New System.Drawing.Size(71, 20)
+        Me.InformatiiToolStripMenuItem.Text = "Informatii"
+        '
+        'IesireToolStripMenuItem
+        '
+        Me.IesireToolStripMenuItem.Name = "IesireToolStripMenuItem"
+        Me.IesireToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.IesireToolStripMenuItem.Text = "Iesire"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Image = Global.Proiect.My.Resources.Resources._5leuti
+        Me.PictureBox1.Location = New System.Drawing.Point(22, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(301, 162)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Timer1
+        '
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(189, 26)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(359, 277)
+        Me.ListBox1.TabIndex = 2
+        Me.ListBox1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Proiect.My.Resources.Resources.bnrbun
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(548, 302)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Meniu Principal"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents JocNouToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ManualToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InformatiiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents IesireToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ReguliToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BancnoteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BaniToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LeuToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LeiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LeiToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LeiToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LeiToolStripMenuItem3 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LeiToolStripMenuItem4 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents SetareValoareToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ Proiect/Form1.vb
@@ -0,0 +1,225 @@
+﻿Public Class Form1
+    Dim moveleft As Boolean = True
+    Dim moveright As Boolean = False
+    Dim valoare As Integer
+    Dim nr As Integer = 8
+    Dim a = New Integer() {0, 1, 5, 10, 50, 100, 200, 500}
+    Dim s As Integer
+    Dim x(101) As Integer
+
+
+
+
+
+
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip1.ItemClicked
+
+    End Sub
+
+    Private Sub IesireToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles IesireToolStripMenuItem.Click
+
+        ListBox1.Visible = False
+
+        PictureBox1.Visible = False
+        Dim answer As MsgBoxResult
+        answer = MsgBox("Doriti sa parasiti Aplicatia?", MsgBoxStyle.YesNo)
+        If answer = MsgBoxResult.Yes Then
+
+            End
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Computer.Audio.Play(My.Resources.Song, AudioPlayMode.Background)
+        Timer1.Start()
+
+
+
+    End Sub
+
+    Private Sub InformatiiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InformatiiToolStripMenuItem.Click
+
+
+        PictureBox1.Visible = False
+        ListBox1.Visible = False
+
+        MessageBox.Show("Proiect realizat de Bujdei Mihai-Dan,3121A")
+    End Sub
+
+    Private Sub BaniToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BaniToolStripMenuItem.Click
+        PictureBox1.Image = My.Resources._50banibun
+
+        PictureBox1.Visible = True
+
+    End Sub
+
+    Private Sub LeuToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LeuToolStripMenuItem.Click
+        PictureBox1.Image = My.Resources._1leut
+        PictureBox1.Visible = True
+
+
+    End Sub
+
+    Private Sub LeiToolStripMenuItem4_Click(sender As Object, e As EventArgs) Handles LeiToolStripMenuItem4.Click
+        PictureBox1.Image = My.Resources._500lei_bun
+        PictureBox1.Visible = True
+
+    End Sub
+
+    Private Sub LeiToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles LeiToolStripMenuItem3.Click
+        PictureBox1.Image = My.Resources._200lei
+        PictureBox1.Visible = True
+
+    End Sub
+
+    Private Sub LeiToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles LeiToolStripMenuItem2.Click
+        PictureBox1.Image = My.Resources._100leibun
+        PictureBox1.Visible = True
+        Timer1.Start()
+
+
+    End Sub
+
+    Private Sub LeiToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles LeiToolStripMenuItem1.Click
+        PictureBox1.Image = My.Resources._50lei
+        PictureBox1.Visible = True
+
+    End Sub
+
+    Private Sub LeiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LeiToolStripMenuItem.Click
+
+        PictureBox1.Image = My.Resources._5leuti
+        PictureBox1.Visible = True
+        ListBox1.Visible = False
+        Timer1.Start()
+
+
+
+    End Sub
+
+    Private Sub ManualToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ManualToolStripMenuItem.Click
+        PictureBox1.Visible = False
+        ListBox1.Visible = False
+
+
+
+    End Sub
+
+    Private Sub ReguliToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReguliToolStripMenuItem.Click
+        PictureBox1.Visible = False
+        ListBox1.Visible = False
+
+
+        MessageBox.Show("Aceasta aplicatie permite utilizatorilor sa introduca o valoare iar aplicatiia va afisa care este solutia optima pentru a plati aceea suma ")
+    End Sub
+
+    Private Sub JocNouToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles JocNouToolStripMenuItem.Click
+        PictureBox1.Visible = False
+
+
+
+
+
+
+        If (valoare = 0) Then
+            MessageBox.Show("Eroare.Introduceti o valoare")
+        Else
+            back(1)
+
+
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub BancnoteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BancnoteToolStripMenuItem.Click
+        Timer1.Start()
+        ListBox1.Visible = False
+
+        PictureBox1.Visible = False
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If moveleft = True Then
+            PictureBox1.Left -= 20
+        End If
+        If PictureBox1.Left <= 0 Then
+            moveleft = False
+            moveright = True
+        End If
+
+        If moveright = True Then
+            PictureBox1.Left += 20
+        End If
+
+        If moveright = True Then
+            PictureBox1.Left += 1
+        End If
+        If PictureBox1.Left >= Me.Height - 60 Then
+            moveleft = True
+            moveright = False
+        End If
+    End Sub
+
+    
+    Private Sub SetareValoareToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SetareValoareToolStripMenuItem1.Click
+       
+        valoare = InputBox("Inserare valaore", "Atentie.Inserati tot timpul o valoare ")
+
+
+
+
+
+    End Sub
+    Public Sub tipar(ByVal k As Integer)
+
+        Dim nrr As Integer
+        Dim t As Integer = valoare + 10
+        ListBox1.Visible = True
+
+        ListBox1.Items.Add("Aveti grija algoritmul calculeaza valoare " & t)
+        ListBox1.Items.Add("Solutia " & " Afisare :")
+        ListBox1.Items.Add("Numarul de bancnote" & "       " & "De  valoarea")
+
+
+
+        For i As Integer = 0 To 7
+            ListBox1.Items.Add(x(i) & "                               " & "de            " & a(i) & " lei")
+
+
+        Next
+
+
+    End Sub
+
+    Public Sub back(ByVal k As Integer)
+        If (s = valoare) Then
+            tipar(k - 1)
+        Else
+            x(k) = -1
+            If (k < nr) Then
+                While ((x(k) * a(k)) + s < valoare And k < nr)
+                    x(k) = x(k) + 1
+                    s = s + (x(k) * a(k))
+                    back(k + 1)
+                    s = s - (x(k) * a(k))
+
+                End While
+
+            End If
+        End If
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
+
